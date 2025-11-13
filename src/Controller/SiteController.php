623bf9be06d10b398386a17fd89e204729d683c3
kdubun <killian.dubun@ynov.com>
2025-11-13 14:16:46 +0100

    projects init

---
+++ src/Controller/SiteController.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class SiteController extends AbstractController
+{
+    #[Route('/', name: 'site_index')]
+    public function index(): Response
+    {
+        return $this->render('site/index.html.twig');
+    }
+
+    #[Route('/notre-equipe', name: 'site_team')]
+    public function team(): Response
+    {
+        return $this->render('site/team.html.twig');
+    }
+}
